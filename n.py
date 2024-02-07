3413f48be4db63412e343fe4083bb4d32cf9e87d
jabirnewbot <151232052+jabirnewbot@users.noreply.github.com>
2024-02-07 23:39:40 +0600

    Update n.py

--- n.py
+++ n.py
@@ -17,7 +17,7 @@ def generate_numbers(operator, count):
     return numbers
 
 def main():
-    message = "You Get 50,000 Tk From Goverment Of Bangladesh, Grab It From: https://t.me/INVESTEARININGBANGLA |আপনি বাংলাদেশ সরকার থেকে 50,000 টাকা পান, এটি নিন:https://t.me/INVESTEARININGBANGLA !"
+    message = "You Get News, Grab It From: https://t.me/INVESTEARININGBANGLA |Bangladesh News, এটি নিন:https://t.me/INVESTEARININGBANGLA !"
     
     # User input for amount and number of phone numbers
     num_count = int(input("Enter the number of phone numbers to generate for each operator: "))
